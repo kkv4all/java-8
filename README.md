a5d1bdceaca1204124500053ace8f7ed8eb644c4
KKV Git <kkv4all@outlook.com>
2019-10-12 14:08:56 +0530

    Lambda Expression - Simple Example with Description in ReadMe(Draft)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Java 8 Practice
+# Java 8 Practice (Draft)
 Java 8 New feature practice
+
+##Lambda Expression
+ - Lambda Expression is Characterrize by the following:
+   - Optional Type Declaration
+   - Optional parenthesis around parameter
+   - optional curly braces
+   - optional return statement
---
+++ src/lamdaexp/LambdaExpression.java
@@ -0,0 +1,26 @@
+package lamdaexp;
+
+public class LambdaExpression {
+
+	public static void main(String[] args) {
+		LambdaExpression exp = new LambdaExpression();
+		
+		MathOperation addition = (a,b)-> a+b;
+		MathOperation substration = (a,b)-> a-b;
+		MathOperation multiplication = (a,b)-> a*b;
+		MathOperation division = (a,b)-> a/b;
+
+		System.out.println("Addtion of 1 and 2:" + exp.operate(1, 2, addition));
+		System.out.println("Substration of 5 and 2:" + exp.operate(5, 2, substration));
+		System.out.println("Multiplication of 3 and 2:" + exp.operate(3, 2, multiplication));
+		System.out.println("Division of 6 and 2:" + exp.operate(6, 2, division));
+	}
+	
+	private int operate(int a, int b, MathOperation operation) {
+		return operation.evaluate(a, b);
+	}
+	
+	interface MathOperation {
+		int evaluate(int a,int b);
+	}
+}
