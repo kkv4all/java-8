f08583a120c5e261c6b98ba42298392d5eabd81e
KKV Git <kkv4all@outlook.com>
2019-10-13 23:31:47 +0530

    Optional Object - Simple Examples with Description in ReadMe(Draft)

---
+++ src/nashorn/Nashorn.java
@@ -0,0 +1,26 @@
+package nashorn;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class Nashorn {
+
+	public static void main(String[] args) {
+		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
+		
+		String name = "World";
+		int result=0;
+		
+		try {
+			nashorn.eval("print('Hello "+name+"!')");
+			result = (int) nashorn.eval("10+2");
+		}catch (ScriptException e) {
+			System.out.println("Error executing script:"+e.getMessage());
+		}
+		
+		System.out.println(result);
+	}
+
+}
